6c9fa4fce29e06dd713e0b37fd756ca8d106f9f0
polubis <polubis@wp.pl>
2021-01-13 11:52:04 +0100

    Add styled components

---
+++ blog/src/components/navbar/Navbar.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Navbar() {
+  return <nav>Hi from navbar</nav>
+}
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
+import Navbar from "../components/navbar/Navbar"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Navbar />
+      Hello world!
+    </div>
+  )
 }
