74ccf19d4c299e36e58dd6ec66a3eb6407b61e12
polubis <polubis@wp.pl>
2021-01-14 20:47:49 +0100

    Add ArticleTitle component

--- blog/src/pages/index.tsx
+++ blog/src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 
 import Layout from "../components/layout/Layout"
+import ArticleTitle from "../components/article-title/ArticleTitle"
 
 export default function (): React.ReactElement {
   return (
     <Layout>
-      <div>Content</div>
+      <div>
+        <ArticleTitle>
+          Tdd o true example we writting our custom library
+        </ArticleTitle>
+      </div>
     </Layout>
   )
 }
