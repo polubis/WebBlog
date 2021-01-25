581aa73cbb7c7c8afb49d965a62d8715fa76bb8c
polubis <polubis@wp.pl>
2021-01-25 11:58:34 +0100

    Add summary section

--- blog/root-wrapper.js
+++ blog/root-wrapper.js
@@ -3,6 +3,7 @@ import { MDXProvider } from "@mdx-js/react"
 
 import Section from "./src/components/article/Section"
 import Code from "./src/components/article/Code"
+import Summary from "./src/components/article/Summary"
 import Link from "./src/components/article/Link"
 import * as Text from "./src/components/article/Text"
 
@@ -10,6 +11,7 @@ const shortcodes = {
   Section,
   Code,
   Link,
+  Summary,
   ...Text,
 }
 
