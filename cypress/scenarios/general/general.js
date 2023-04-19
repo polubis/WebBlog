96355515d7ea5527eb0d3078c6744ac612209d8a
klaudiazalewska <kaza2809@gmail.com>
2023-04-19 20:10:23 +0200

    Add e2e tests for top naviagtion bar on desktop

--- cypress/scenarios/general/general.js
+++ cypress/scenarios/general/general.js
@@ -4,11 +4,31 @@ describe("Navigation bar", () => {
     cy.viewport(1650, 795)
   })
 
-  it("Navigation bar at top works as expected", () => {
-    cy.get(`#home-navigation-link-authors`).should(
-      "have.attr",
-      "href",
-      "/authors/"
-    )
+  it("should navigate to the articles' page", () => {
+    cy.get(`#home-navigation-link-Articles`)
+      .should("have.attr", "href", "/articles/")
+      .click()
+    cy.url().should("include", "/articles")
+  })
+
+  it("should navigate to the authors' page", () => {
+    cy.get(`#home-navigation-link-Authors`)
+      .should("have.attr", "href", "/authors/")
+      .click()
+    cy.url().should("include", "/authors")
+  })
+
+  it("should navigate to the courses' page", () => {
+    cy.get(`#home-navigation-link-Courses`)
+      .should("have.attr", "href", "/courses/")
+      .click()
+    cy.url().should("include", "/courses")
+  })
+
+  it("should navigate to the creator's page", () => {
+    cy.get(`#home-navigation-link-Creator`)
+      .should("have.attr", "href", "/blog-creator/")
+      .click()
+    cy.url().should("include", "/blog-creator")
   })
 })
