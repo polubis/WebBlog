c3a84e31307bea196f9f18d8ac3c6fa2e48b1d2d
klaudiazalewska <kaza2809@gmail.com>
2023-05-01 21:31:52 +0200

    Add join button automated test

---
+++ cypress/scenarios/general/join-button.js
@@ -0,0 +1,24 @@
+describe("Join button in the top navigation bar", () => {
+  it("Join button in the top navigation bar works as expected", () => {
+    cy.visit("http://localhost:8000/")
+    cy.viewport(1650, 795)
+
+    cy.get("#home-navigation-join-button").contains("Join").click()
+    cy.get('[type="email"]').type("example@mail.com")
+    cy.get("textarea").type("Some text")
+    cy.get('[type="submit"]').contains("SEND").click()
+    // cy.get(".ok").then(function (el) {
+    //   const actualText = el.text()
+    //   expect(
+    //     actualText.includes(
+    //       "We have your submission! We will get back to you within 2 days"
+    //     )
+    //   )
+    // })
+    cy.get(".ok").should(
+      "have.text",
+      "We have your submission! We will get back to you within 2 days"
+    )
+    cy.get('[type="submit"]').contains("CLOSE").click()
+  })
+})
