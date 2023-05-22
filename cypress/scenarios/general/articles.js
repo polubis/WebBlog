b46cd911d0c11c1de951384e426a2707665baa3b
klaudiazalewska <73396554+klaudiazalewska@users.noreply.github.com>
2023-05-22 18:27:27 +0200

    122 add e2e tests for navigation (#229)
    * Add e2e tests for top naviagtion bar on desktop
    
    * Add join button automated test
    
    * Add 'check blog' and 'write your first article' buttons' automated tests in main page automated tests
    
    * Add about us section automated test in main page's automated tests and update join button automated test
    
    * Update join button automated test and main page automated tests
    
    * Add 'recent articles' section' automated test
    
    * Add articles; automated tests
    
    * Add authors' page automated tests
    
    * Update articles' automated tests
    
    * Add breadcrumbs on desktop automated tests
    
    * Add breadcrumbs on mobile automated tests
    
    * Add code snippets button's automated test
    
    * Add courses navigation's automated tests
    
    * Add classes
    
    * Update main-page automated tests
    
    * Update main page automated tests - add tests for stats section
    
    * Add automated tests for stats section in footer
    
    * Update main page tests
    
    ---------
    
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

---
+++ cypress/scenarios/general/articles.js
@@ -0,0 +1,22 @@
+describe("Articles' tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8000/")
+    cy.viewport(1650, 795)
+  })
+
+  it('"Read article" buttons work as expected', () => {
+    cy.get("#home-navigation-link-Articles").click()
+    cy.url().should("include", "articles")
+    cy.get(".Grid-fPzpTg > :nth-child(1) > [href]").click()
+    //Add tests for each article
+  })
+
+  it("Clicking on author's avatar works as expected", () => {
+    cy.get("#home-navigation-link-Articles").click()
+    cy.url().should("include", "articles")
+    cy.get(
+      ":nth-child(1) > .Details___default-McGbG > a > .AuthorBadge__Badge-gJdzZR > .AuthorAvatar-jsiITc > .gatsby-image-wrapper > picture > img"
+    ).click()
+    cy.url().should("include", "authors")
+  })
+})
---
+++ cypress/scenarios/general/breadcrumbs-desktop.js
@@ -0,0 +1,32 @@
+describe("Breadcrumbs on desktop tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8000/")
+    cy.viewport(1650, 795)
+  })
+
+  it("View breadcrumbs in the articles' page", () => {
+    cy.get("#home-navigation-link-Articles").click()
+    cy.url().should("include", "articles")
+    cy.get(".Grid-fPzpTg > :nth-child(1) > [href]").click()
+    cy.get(".Breadcrumbs__Container-eJMSmD > :nth-child(2) > a").click()
+    cy.url().should("include", "articles")
+    /*Add for each article*/
+  })
+
+  it("View breadcrumbs in the courses' page", () => {
+    cy.get("#home-navigation-link-Courses").click()
+    cy.url().should("include", "courses")
+    cy.get(":nth-child(1) > a > button").eq(0).click()
+    cy.get(".Breadcrumbs__Container-eJMSmD > :nth-child(2) > a").click()
+    cy.url().should("include", "courses")
+    /*Add for each course*/
+  })
+
+  it("View breadcrumbs in the lessons", () => {
+    cy.get("#home-navigation-link-Courses").click()
+    cy.url().should("include", "courses")
+    cy.get(":nth-child(1) > a > button").eq(0).click()
+    cy.get("header > [href] > button").eq(1).click()
+    /*Add for each lesson for each course*/
+  })
+})
