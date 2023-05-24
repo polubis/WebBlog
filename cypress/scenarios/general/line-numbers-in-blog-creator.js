2d552a54848235e78d7c00e9291d35ea99f37313
klaudiazalewska <kaza2809@gmail.com>
2023-05-24 20:28:09 +0200

    Add line numbers in the blog creator automated tests

---
+++ cypress/scenarios/general/line-numbers-in-blog-creator.js
@@ -0,0 +1,17 @@
+describe("Community links in the navigation tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8000/")
+    cy.viewport(1650, 795)
+  })
+
+  it("In the article preview on the creator page, there are line numbers", () => {
+    cy.get("#home-navigation-link-Creator").click()
+    cy.wait(4000)
+    cy.url().should("include", "/blog-creator")
+    cy.get("pre")
+      .children()
+      .should("have.class", "editor-line-wrapper")
+      .children()
+      .should("have.class", "editor-line-number")
+  })
+})
