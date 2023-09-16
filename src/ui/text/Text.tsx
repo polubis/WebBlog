7c8b1a63abf79bfc1ce3e379c3d186eeb0d95dcb
polubis <polubis@wp.pl>
2023-09-16 16:05:02 +0200

    [PATCH] Fix fonts when content is too big

--- src/ui/text/Text.tsx
+++ src/ui/text/Text.tsx
@@ -99,6 +99,7 @@ export const Huge = styled.h1<Props>`
 
 const XXL = styled.h1<Props>`
   font-size: 32px;
+  word-break: break-word;
   font-weight: ${(props: Props) => getWeight(props, Weight.BOLD)};
   ${style}
 `
@@ -106,6 +107,7 @@ const XXL = styled.h1<Props>`
 export const XL = styled.h2<Props>`
   font-size: 26px;
   line-height: 32px;
+  word-break: break-word;
   font-weight: ${(props: Props) => getWeight(props, Weight.BOLD)};
   ${style}
 `
