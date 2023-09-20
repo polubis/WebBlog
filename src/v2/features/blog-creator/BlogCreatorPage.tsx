e78c9769670bd62cfca10bc0890d25d82e4a1a56
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-09-20 11:49:52 +0200

    [PATCH] Improve lazy loading on blog creator page

--- src/v2/features/blog-creator/BlogCreatorPage.tsx
+++ src/v2/features/blog-creator/BlogCreatorPage.tsx
@@ -1,15 +1,21 @@
 import React from "react"
 
 import { LayoutProvider } from "../../providers/LayoutProvider"
-import { BlogCreatorPageModel, Layout } from "../../core/models"
+import { BlogCreatorPageModel, Layout as LayoutModal } from "../../core/models"
 import { SEO } from "../../containers/SEO"
-import { BlogCreatorPageProvider } from "./BlogCreatorPageProvider"
-import { BlogCreatorView } from "./BlogCreatorView"
+import Loadable from 'react-loadable'
+import Layout from "../../containers/Layout"
+import { Content } from "../../../ui"
+
+const BlogCreatorView = Loadable({
+  loader: () => import('./BlogCreatorView').then(m => m.BlogCreatorView),
+  loading: () => null,
+})
 
 interface BlogCreatorPageProps {
   pageContext: {
     creator: BlogCreatorPageModel
-    layout: Layout
+    layout: LayoutModal
   }
 }
 
@@ -18,18 +24,20 @@ const BlogCreatorPage = ({ pageContext }: BlogCreatorPageProps) => {
 
   return (
     <LayoutProvider initialState={layout}>
-      <BlogCreatorPageProvider initialState={creator}>
-        <SEO
-          ga_page={creator.ga_page}
-          url={creator.url}
-          title={`${layout.site_name} ${creator.t.page_title}`}
-          type="website"
-          image={layout.site_url + "/icon-192x192.png"}
-          description={creator.t.page_description}
-        >
-          <BlogCreatorView />
-        </SEO>
-      </BlogCreatorPageProvider>
+      <Layout>
+        <Content paddingY>
+          <SEO
+            ga_page={creator.ga_page}
+            url={creator.url}
+            title={`${layout.site_name} ${creator.t.page_title}`}
+            type="website"
+            image={layout.site_url + "/icon-192x192.png"}
+            description={creator.t.page_description}
+          >
+            <BlogCreatorView {...creator} />
+          </SEO>
+        </Content>
+      </Layout>
     </LayoutProvider>
   )
 }
