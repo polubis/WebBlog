956456fdc2127ed4714be810e7d0bedc9ce1db9b
Adrian Połubiński <polubis@wp.pl>
2023-09-20 13:13:03 +0200

    [PATCH] Remove not used code (#483)
    * [PATCH] Remove not needed code
    
    * [PATCH] Improve lazy loading on blog creator page
    
    ---------
    
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- src/utils/timeCalculator.ts
+++
@@ -1,7 +0,0 @@
-import { formatDistance } from "date-fns"
-
-export const getDistanceFromNow = (date: Date): string => {
-  const now = new Date()
-  const distance = formatDistance(now, date, { includeSeconds: true })
-  return distance
-}
--- src/v2/ui/image/Image.tsx
+++ src/v2/ui/image/Image.tsx
@@ -1,66 +1,64 @@
-import React, { memo, useEffect } from "react"
+import React, { useEffect } from "react"
 import type { ImageProps } from "./models"
 import { useToggle } from "../../utils/useToggle"
 import { useImageLoad } from "../../utils/useImageLoad"
 import { ImageWrapper } from "./ImageWrapper"
 import { ImageContent } from "./ImageContent"
 
-const Image = memo(
-  ({
-    className,
-    src,
-    description,
-    rolled = false,
-    title,
-    alt,
-    Roller,
-    Loading,
-    Error,
-  }: ImageProps) => {
-    const toggler = useToggle({ opened: !rolled })
-    const [image, loadImage] = useImageLoad()
+const Image = ({
+  className,
+  src,
+  description,
+  rolled = false,
+  title,
+  alt,
+  Roller,
+  Loading,
+  Error,
+}: ImageProps) => {
+  const toggler = useToggle({ opened: !rolled })
+  const [image, loadImage] = useImageLoad()
 
-    useEffect(() => {
-      loadImage(src)
-    }, [])
+  useEffect(() => {
+    loadImage(src)
+  }, [])
 
-    const LoaderNode = (
-      <ImageWrapper description={description}>
-        <Loading />
-      </ImageWrapper>
-    )
-    const ErrorNode = (
-      <ImageWrapper description={description}>
-        <Error />
-      </ImageWrapper>
-    )
+  const LoaderNode = (
+    <ImageWrapper description={description}>
+      <Loading />
+    </ImageWrapper>
+  )
+  const ErrorNode = (
+    <ImageWrapper description={description}>
+      <Error />
+    </ImageWrapper>
+  )
 
-    if (toggler.opened) {
-      if (image.type === "idle" || image.type === "pending") {
-        return LoaderNode
-      }
-
-      if (image.type === "fail") return ErrorNode
-
-      return (
-        <ImageContent
-          src={image.data}
-          description={description}
-          title={title}
-          alt={alt}
-          className={className}
-        />
-      )
+  if (toggler.opened) {
+    if (image.type === "idle" || image.type === "pending") {
+      return LoaderNode
     }
 
+    if (image.type === "fail") return ErrorNode
+
     return (
-      // The case if user scrolled to image section,
-      // but image is really big. So we make it toggleable.
-      <ImageWrapper description={description}>
-        <Roller {...toggler} />
-      </ImageWrapper>
+      <ImageContent
+        src={image.data}
+        description={description}
+        title={title}
+        alt={alt}
+        className={className}
+      />
     )
   }
-)
+
+  return (
+    // The case if user scrolled to image section,
+    // but image is really big. So we make it toggleable.
+    <ImageWrapper description={description}>
+      <Roller {...toggler} />
+    </ImageWrapper>
+  )
+}
 
 export { Image }
