1e8250ad7e42deeb945e1b051689ac49a612ce3f
polubis <polubis@wp.pl>
2023-09-23 09:16:54 +0200

    [PATCH] Remove lazy analytics and improve code_error

--- src/v2/containers/CodeErrorWrapper.tsx
+++ src/v2/containers/CodeErrorWrapper.tsx
@@ -2,12 +2,25 @@ import React, { useEffect } from "react"
 import { CodePlaceholder } from "./CodePlaceholder"
 import { CodeErrorWrapperProps } from "./models"
 import { useAnalytics } from "../../utils/useAnalytics"
+import { useLayoutProvider } from "../providers/LayoutProvider"
 
 const CodeErrorWrapper = ({ src }: CodeErrorWrapperProps) => {
-  const { track } = useAnalytics()
+  const { trackFullEvent } = useAnalytics()
+  const layout = useLayoutProvider()
 
   useEffect(() => {
-    track({ name: "rendering_code_error", link: src })
+    const url = window.location.pathname + window.location.search
+
+    if (url.includes(layout.routes.creator.to)) {
+      return
+    }
+
+    trackFullEvent({
+      name: "rendering_code_error",
+      category: "errors",
+      src,
+      url: window.location.pathname + window.location.search,
+    })
   }, [])
 
   return <CodePlaceholder label="smth_wrong" />
