8bdf8211338cb52614dc653202341c152d299e49
polubis <polubis@wp.pl>
2023-09-23 09:19:41 +0200

    [PATCH] Improve image_error event

--- src/v2/containers/ImageErrorWrapper.tsx
+++ src/v2/containers/ImageErrorWrapper.tsx
@@ -2,12 +2,25 @@ import React, { useEffect } from "react"
 import { ImagePlaceholder } from "./ImagePlaceholder"
 import type { ImageErrorWrapperProps } from "./models"
 import { useAnalytics } from "../../utils/useAnalytics"
+import { useLayoutProvider } from "../providers/LayoutProvider"
 
 const ImageErrorWrapper = ({ src }: ImageErrorWrapperProps) => {
-  const { track } = useAnalytics()
+  const { trackFullEvent } = useAnalytics()
+  const layout = useLayoutProvider()
 
   useEffect(() => {
-    track({ name: "rendering_image_error", link: src })
+    const url = window.location.pathname + window.location.search
+
+    if (url.includes(layout.routes.creator.to)) {
+      return
+    }
+
+    trackFullEvent({
+      name: "rendering_image_error",
+      src,
+      category: "errors",
+      url,
+    })
   }, [])
 
   return <ImagePlaceholder label="smth_wrong" />
