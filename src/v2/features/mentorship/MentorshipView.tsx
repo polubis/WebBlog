cfb64fd5393cf199cf3461ef53bc7b4d50c5cb78
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-11-22 11:58:48 +0100

    wip

--- src/v2/features/mentorship/MentorshipView.tsx
+++ src/v2/features/mentorship/MentorshipView.tsx
@@ -1,11 +1,13 @@
-import React from "react"
+import React, { useState, useMemo } from "react"
 import Layout from "../../containers/Layout"
-import { Content, RightArrowIcon, S, X, XL, XXL } from "../../../ui"
+import { Content, M, RightArrowIcon, S, X, XL, XXL } from "../../../ui"
 import { useLayoutProvider } from "../../providers/LayoutProvider"
 import styled from "styled-components"
 import { Link } from "gatsby"
 import { useMentorshipPageProvider } from "./MentorshipPageProvider"
 import { L_UP, T_UP } from "../../../utils/viewport"
+import { Tabs } from "../../ui/tabs/Tabs"
+import { Tab } from "../../ui/tabs/Tab"
 
 const Container = styled.main`
   & > header {
@@ -20,11 +22,18 @@ const Container = styled.main`
     }
   }
 
-  & > section {
+  .mentorship-view-plans-select {
+    margin: 40px 0 20px 0;
+
+    .tab {
+      padding: 8px 12px;
+    }
+  }
+
+  .mentorship-view-plans {
     display: grid;
     gap: 28px;
     justify-content: center;
-    margin-top: 40px;
     grid-template-columns: minmax(1fr, 320px);
 
     @media ${T_UP} {
@@ -95,6 +104,7 @@ const Container = styled.main`
 const MentorshipView = () => {
   const layout = useLayoutProvider()
   const mentorship = useMentorshipPageProvider()
+  const [activePlan, setActivePlan] = useState(mentorship.t.plans[0])
 
   return (
     <Layout>
@@ -104,8 +114,21 @@ const MentorshipView = () => {
             <XXL>{mentorship.t.heading}</XXL>
             <X>{mentorship.t.heading_description}</X>
           </header>
-          <section>
-            {mentorship.t.subscriptions.map(subscription => (
+          <section className="mentorship-view-plans-select center">
+            <Tabs>
+              {mentorship.t.plans.map(plan => (
+                <Tab
+                  key={plan.key}
+                  active={plan.key === activePlan.key}
+                  onClick={() => setActivePlan(plan)}
+                >
+                  <M>{plan.label}</M>
+                </Tab>
+              ))}
+            </Tabs>
+          </section>
+          <section className="mentorship-view-plans">
+            {activePlan.content.map(subscription => (
               <div
                 className="mentorship-card free"
                 key={subscription.label}
@@ -118,7 +141,9 @@ const MentorshipView = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <button className="upper button primary">Try it</button>
+                  <button className="upper button secondary">
+                    {layout.t.try_it}
+                  </button>
                 </Link>
                 <ul>
                   {subscription.bullets.map(bullet => (
