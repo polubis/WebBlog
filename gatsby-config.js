3d90749d9886b34ddbbaf9112d77f21a9a71323b
Adrian Połubiński <adrian.polubinski@billennium.com>
2023-12-11 10:46:11 +0100

    [REVERT] Redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-no-sourcemaps",
-    "gatsby-plugin-netlify",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
