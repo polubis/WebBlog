3b26fd335f6540ecc42b5f824ed3f47e87435c61
Adrian Połubiński <polubis@wp.pl>
2023-12-11 11:25:39 +0100

    [PATCH] Add custom error screen and redirections (#549)
    Co-authored-by: Adrian Połubiński <adrian.polubinski@billennium.com>

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,47 @@
 import React from "react"
+import Section from "../components/article/Section"
+import { M, XL } from "../ui"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { useLang } from "../v2/core/useLang"
+import t_404_en from "../v2/translation/404/en.json"
+import t_404_pl from "../v2/translation/404/pl.json"
 
-export default function (): React.ReactElement {
-  return <div>404</div>
+const lookup = {
+  pl: t_404_pl,
+  en: t_404_en,
+}
+
+const Container = styled.div`
+  height: 100vh;
+
+  .col {
+    margin: auto;
+    max-width: 400px;
+    padding: 20px;
+  }
+
+  a {
+    margin-top: 20px;
+  }
+`
+
+export default function () {
+  const lang = useLang()
+
+  if (!lang) return null
+
+  const content = lookup[lang]
+
+  return (
+    <Container className="center">
+      <Section className="col">
+        <XL>{content.heading}</XL>
+        <M>{content.description}</M>
+        <Link className="button upper primary" to="/">
+          {content.home_page}
+        </Link>
+      </Section>
+    </Container>
+  )
 }
