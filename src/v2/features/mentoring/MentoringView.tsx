57d6e7a29added9e727ca0f4974f963b9262daef
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-02-23 08:35:54 +0100

    wip

--- src/v2/features/mentoring/MentoringView.tsx
+++ src/v2/features/mentoring/MentoringView.tsx
@@ -9,19 +9,20 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   grid-template-areas:
-    "mentoring-general mentoring-general mentoring-single-call mentoring-single-call mentoring-single-call"
-    "mentoring-tech-talk mentoring-tech-talk a a a"
+    "mentoring-first mentoring-first mentoring-second mentoring-second mentoring-second"
+    "mentoring-third mentoring-third mentoring-third mentoring-fourth mentoring-fourth"
     "b b c c c";
   gap: 20px;
 `
 
 const Container = styled.div`
+  margin-bottom: 48px;
+
   ${XL} {
     margin-bottom: 20px;
   }
 
   .mentoring-general {
-    margin-bottom: 40px;
     background: none;
     padding: 0;
     max-width: 700px;
@@ -64,12 +65,20 @@ const Tile = styled.div`
     }
   }
 
-  &.mentoring-single-call {
-    grid-area: mentoring-single-call;
+  &.mentoring-second {
+    grid-area: mentoring-second;
   }
 
-  &.mentoring-tech-talk {
-    grid-area: mentoring-tech-talk;
+  &.mentoring-fourth {
+    grid-area: mentoring-fourth;
+  }
+
+  &.mentoring-first {
+    grid-area: mentoring-first;
+  }
+
+  &.mentoring-third {
+    grid-area: mentoring-third;
   }
 `
 
@@ -81,18 +90,17 @@ const MentoringView = () => {
       <Content paddingY>
         <Container>
           <Tile className="mentoring-general">
-            <XXL>
-              Mentoring dopasowany do Twoich potrzeb
-            </XXL>
+            <XXL>Mentoring dopasowany do Twoich potrzeb</XXL>
             <M>
               Wybierz odpowiedni <B>plan</B>, <B>sciężkę</B> lub{" "}
-              <B>konsultację</B>.
+              <B>konsultację</B>, a My zajmiemy się resztą.
             </M>
             <M>
               <B>Obszary</B>: Frontend, Backend, Testowanie manualne, Testowanie
               automatyczne, Narzędzia AI, Umiejętności miękkie, Architektura,
               Automatyzacja zadań, UX/UI, SEO, POC, Tworzenie aplikacji,
-              Tworzenie stron internetowych.
+              Tworzenie stron internetowych, Analytics, Testy A/B, Bazy danych,
+              Pierwsza praca w IT, Zarządzanie projektami.
             </M>
             <footer>
               <button className="button upper primary">
@@ -100,9 +108,31 @@ const MentoringView = () => {
               </button>
             </footer>
           </Tile>
-          <XL>Popularne</XL>
+        </Container>
+        <Container>
+          <XL>Zlecenia</XL>
           <Grid>
-            <Tile className="mentoring-single-call">
+            <Tile className="mentoring-first">
+              <X>Tworzenie artykułów</X>
+              <M>
+                Tworzymy artykuły na Twoje zlecenie. Wybierasz temat, a My
+                zajmiemy się resztą.
+              </M>
+              <M>
+                ✔️ Do 3 poprawek, ✔️ Dowolny temat, ✔️ Dowolny format, ✔️ Zwrot
+                pieniędzy
+              </M>
+              <M>
+                Koszt: <B>123 zł</B> (zawiera podatek VAT).
+              </M>
+              <footer>
+                <button className="button upper primary">Zapisz się</button>
+                <button className="button upper primary">
+                  Więcej informacji
+                </button>
+              </footer>
+            </Tile>
+            <Tile className="mentoring-second">
               <X>Pojedyńcza konsultacja</X>
               <M>
                 Potrzebujesz pomocy przy zadaniu? Nie rozumiesz zagadnienia?
@@ -114,7 +144,7 @@ const MentoringView = () => {
               </M>
               <M>
                 ✔️ Nagrywanie, ✔️ 1 godzina, ✔️ Dostęp do materiałów, ✔️ Max 3
-                osoby
+                osoby, ✔️ Zwrot pieniędzy
               </M>
               <M>
                 Koszt: <B>246 zł</B> (zawiera podatek VAT).
@@ -126,7 +156,7 @@ const MentoringView = () => {
                 </button>
               </footer>
             </Tile>
-            <Tile className="mentoring-tech-talk">
+            <Tile className="mentoring-fourth">
               <X>Rozmowa techniczna</X>
               <M>
                 Szukasz nowej pracy lub chcesz zmienić obecną, ale nie jesteś
@@ -136,10 +166,120 @@ const MentoringView = () => {
               </M>
               <M>
                 ✔️ 1.5 godziny, ✔️ Dostęp do materiałów, ✔️ Max 3 osoby, ✔️
-                Notatka po spotkaniu, ✔️ Nagrywanie
+                Notatka po spotkaniu, ✔️ Nagrywanie, ✔️ Zwrot pieniędzy
               </M>
               <M>
                 Koszt: <B>307,50 zł</B> (zawiera podatek VAT).
+              </M>
+              <footer>
+                <button className="button upper primary">Zapisz się</button>
+                <button className="button upper primary">
+                  Więcej informacji
+                </button>
+              </footer>
+            </Tile>
+            <Tile className="mentoring-third">
+              <X>Tworzenie funkcjonalności</X>
+              <M>
+                Masz aplikację i potrzebujesz nowej funkcjonalności? Odezwij się
+                do Nas i problem załatwiony. Wycenimy funkcjonalność
+                indywidualnie i stworzymy ją od początku do końca wraz z testami
+                automatycznymi i dokumentacją, pilnując najlepszych praktyk.
+              </M>
+              <M>
+                ✔️ Zwrot pieniędzy, ✔️ Testy automatyczne, ✔️ Dokumentacja, ✔️
+                Wysoki standard
+              </M>
+              <M>
+                Koszt: <B>Wycena indwywidualna</B>
+              </M>
+              <footer>
+                <button className="button upper primary">
+                  Poproś o wycenę
+                </button>
+                <button className="button upper primary">
+                  Więcej informacji
+                </button>
+              </footer>
+            </Tile>
+          </Grid>
+        </Container>
+        <Container>
+          <XL>Ścieżki rozwoju</XL>
+          <Grid>
+            <Tile className="mentoring-first">
+              <X>Frontend developer</X>
+              <M>
+                Dowiesz się jak stawiać projekty od zera, nauczysz się tworzyć
+                skalowalny i łatwy w utrzymaniu interfejs użytkownika.
+              </M>
+              <M>
+                ✔️ Materiały, ✔️ Plan rozwoju, ✔️ Praca nad prawdziwym
+                projektem, ✔️ Cykliczne spotkania, ✔️ Live coding, ✔️ Możliwość
+                nagrywania, ✔️ Kurs (VoD), ✔️ Dostęp do prywatnej grupy, ✔️ 3
+                miesiące, ✔️ Zwrot pieniędzy
+              </M>
+              <M>
+                <B>
+                  Next, Gatsby, TypeScript, Cypress, Tailwind, NX, Angular,
+                  React, JavaScript, GraphQL
+                </B>
+              </M>
+              <M>
+                Koszt: <B>1230zł / miesiąc</B> (zawiera podatek VAT).
+              </M>
+              <footer>
+                <button className="button upper primary">Zapisz się</button>
+                <button className="button upper primary">
+                  Więcej informacji
+                </button>
+              </footer>
+            </Tile>
+            <Tile className="mentoring-second">
+              <X>Fullstack</X>
+              <M>
+                Frontend czy Backend będzie Ci nie straszny. Dobierzemy Ci
+                odpowiedni plan rozwoju, który podniesie Twoje kompetencje.
+              </M>
+              <M>
+                ✔️ Materiały, ✔️ Plan rozwoju, ✔️ Praca nad prawdziwym
+                projektem, ✔️ Cykliczne spotkania, ✔️ Live coding, ✔️ Możliwość
+                nagrywania, ✔️ Kurs (VoD), ✔️ Dostęp do prywatnej grupy, ✔️ 3
+                miesiące, ✔️ Zwrot pieniędzy
+              </M>
+              <M>
+                <B>
+                  Next, Gatsby, Firebase, Node, Google Cloud, TypeScript,
+                  Cypress, Tailwind, NX, Angular, React, JavaScript, GraphQL
+                </B>
+              </M>
+              <M>
+                Koszt: <B>1230zł / miesiąc</B> (zawiera podatek VAT).
+              </M>
+              <footer>
+                <button className="button upper primary">Zapisz się</button>
+                <button className="button upper primary">
+                  Więcej informacji
+                </button>
+              </footer>
+            </Tile>
+          </Grid>
+        </Container>
+        <Container>
+          <XL>Dla firm</XL>
+          <Grid>
+            <Tile className="mentoring-first">
+              <X>Tworzenie artykułów</X>
+              <M>
+                Tworzymy artykuły na Twoje zlecenie. Wybierasz temat, a My
+                zajmiemy się resztą.
+              </M>
+              <M>
+                ✔️ Do 3 poprawek, ✔️ Dowolny temat, ✔️ Dowolny format, ✔️ Zwrot
+                pieniędzy
+              </M>
+              <M>
+                Koszt: <B>123 zł</B> (zawiera podatek VAT).
               </M>
               <footer>
                 <button className="button upper primary">Zapisz się</button>
